e9b0fc36de9a88f7367f670ef7563f9871a0e27e
Jesús Borrero <jesufer2018@hotmail.com>
2023-01-10 00:42:18 -0500

    Add Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { TaskListComponent } from './pages/task-list/task-list.component'
+import { AboutComponent } from "./pages/about/about.component"
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: TaskListComponent },
+  { path: "about", component: AboutComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
