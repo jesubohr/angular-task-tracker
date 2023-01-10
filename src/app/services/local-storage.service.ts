e7aed4a7ba3e75d2a94e58899610e9c83664c979
Jesús Borrero <jesufer2018@hotmail.com>
2023-01-10 00:16:01 -0500

    Add localstorage service

---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() { }
+
+  public setItem(key: string, data: any) {
+    localStorage.setItem(key, JSON.stringify(data))
+  }
+
+  public getItem(key: string) {
+    return JSON.parse(localStorage.getItem(key) || "")
+  }
+
+  public removeItem(key: string) {
+    localStorage.removeItem(key)
+  }
+
+  public clear() {
+    localStorage.clear()
+  }
+}
