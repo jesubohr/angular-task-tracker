6cc4c7e9d816f863cd7deac8d57863b948ecfe9a
Jesús Borrero <jesufer2018@hotmail.com>
2023-01-10 00:41:47 -0500

    Update App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { LocalStorageService } from "src/app/services/local-storage.service"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'task-tracker';
+export class AppComponent implements OnInit {
+  constructor (private localService: LocalStorageService) { }
+
+  ngOnInit (): void {
+    const localTheme = this.localService.getItem('tracker.theme')
+    const localThemeHue = this.localService.getItem('tracker.hue')
+
+    if (!localTheme || !localThemeHue) this.setDefaultTheme()
+  }
+
+  setDefaultTheme (): void {
+    const preferredTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+    this.localService.setItem('tracker.theme', preferredTheme)
+    this.localService.setItem('tracker.hue', 270)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from "@angular/common/http"
+import { FormsModule } from "@angular/forms"
+import { NgIconsModule } from "@ng-icons/core"
+import {
+  heroPlus,
+  heroCheck, heroPencilSquare, heroTrash,
+  heroXMark, heroMoon, heroSun, heroSwatch
+} from "@ng-icons/heroicons/outline"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { ButtonComponent } from './components/button/button.component'
+import { TaskItemComponent } from './components/task-item/task-item.component'
+import { TaskFormComponent } from './components/task-form/task-form.component'
+import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher.component'
+import { TaskListComponent } from './pages/task-list/task-list.component'
+import { AboutComponent } from './pages/about/about.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    TaskItemComponent,
+    TaskFormComponent,
+    ThemeSwitcherComponent,
+    TaskListComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgIconsModule.withIcons({
+      heroPlus,
+      heroCheck, heroPencilSquare, heroTrash,
+      heroXMark, heroMoon, heroSun, heroSwatch
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
